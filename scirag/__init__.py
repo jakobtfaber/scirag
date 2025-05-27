e69fb92485fa7c106173e6946fbe2ad3267e8ac9
AprilQx <xxq_010223@163.com>
2025-05-27 13:13:02 +0100

    implementation of paperqa2 as scirag

--- scirag/__init__.py
+++ scirag/__init__.py
@@ -1,9 +1,10 @@
 from .scirag_vertexai import SciRagVertexAI
 from .scirag_openai import SciRagOpenAI
-from .scirag_gemini import SciRagGeminiAI
 from .scirag import SciRag
+from .scirag_paperqa2 import SciRagPaperQA2
 from .dataset import SciRagDataSet
 from .config import REPO_DIR, TOP_K, DISTANCE_THRESHOLD, OAI_PRICE1K
+from .scirag_gemini import SciRagGeminiAI
 
-__all__ = ['SciRagVertexAI', 'SciRagOpenAI', 'REPO_DIR', 'SciRagDataSet', 'SciRag', 'TOP_K', 'DISTANCE_THRESHOLD', 'OAI_PRICE1K', 'SciRagGeminiAI']
+__all__ = ['SciRagVertexAI', 'SciRagOpenAI', 'SciRagPaperQA2', 'REPO_DIR', 'SciRagDataSet', 'SciRag', 'TOP_K', 'DISTANCE_THRESHOLD', 'OAI_PRICE1K','SciRagGeminiAI']
 
