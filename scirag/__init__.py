ba51cb25df0453d872db938d7ef763ac3dd02bf1
borisbolliet <boris.bolliet@gmail.com>
2025-05-27 09:53:35 +0100

    gemini custom rag

--- scirag/__init__.py
+++ scirag/__init__.py
@@ -1,8 +1,9 @@
 from .scirag_vertexai import SciRagVertexAI
 from .scirag_openai import SciRagOpenAI
+from .scirag_gemini import SciRagGeminiAI
 from .scirag import SciRag
 from .dataset import SciRagDataSet
 from .config import REPO_DIR, TOP_K, DISTANCE_THRESHOLD, OAI_PRICE1K
 
-__all__ = ['SciRagVertexAI', 'SciRagOpenAI', 'REPO_DIR', 'SciRagDataSet', 'SciRag', 'TOP_K', 'DISTANCE_THRESHOLD', 'OAI_PRICE1K']
+__all__ = ['SciRagVertexAI', 'SciRagOpenAI', 'REPO_DIR', 'SciRagDataSet', 'SciRag', 'TOP_K', 'DISTANCE_THRESHOLD', 'OAI_PRICE1K', 'SciRagGeminiAI']
 
