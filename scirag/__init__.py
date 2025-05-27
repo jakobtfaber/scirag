b1ed46ba32c389386a49e3f588f155b7e4c98e3d
AprilQx <xxq_010223@163.com>
2025-05-27 16:20:01 +0100

    updated with ocr class

--- scirag/__init__.py
+++ scirag/__init__.py
@@ -5,6 +5,7 @@ from .scirag_paperqa2 import SciRagPaperQA2
 from .dataset import SciRagDataSet
 from .config import REPO_DIR, TOP_K, DISTANCE_THRESHOLD, OAI_PRICE1K
 from .scirag_gemini import SciRagGeminiAI
+from .ocr import MistralOCRProcessor
 
-__all__ = ['SciRagVertexAI', 'SciRagOpenAI', 'SciRagPaperQA2', 'REPO_DIR', 'SciRagDataSet', 'SciRag', 'TOP_K', 'DISTANCE_THRESHOLD', 'OAI_PRICE1K','SciRagGeminiAI']
+__all__ = ['SciRagVertexAI', 'SciRagOpenAI', 'SciRagPaperQA2', 'REPO_DIR', 'SciRagDataSet', 'SciRag', 'TOP_K', 'DISTANCE_THRESHOLD', 'OAI_PRICE1K','SciRagGeminiAI','MistralOCRProcessor']
 
