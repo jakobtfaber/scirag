d2667507a8128e82775947eacfb7b8f65edc9167
AprilQx <xxq_010223@163.com>
2025-05-28 20:38:31 +0100

    updated hybrid openAI embedding and gemini embeddings

--- scirag/__init__.py
+++ scirag/__init__.py
@@ -4,8 +4,8 @@ from .scirag import SciRag
 from .scirag_paperqa2 import SciRagPaperQA2
 from .dataset import SciRagDataSet
 from .config import REPO_DIR, TOP_K, DISTANCE_THRESHOLD, OAI_PRICE1K
-from .scirag_gemini import SciRagGeminiAI
+from .scirag_hybrid import SciRagHybrid
 from .ocr import MistralOCRProcessor
 
-__all__ = ['SciRagVertexAI', 'SciRagOpenAI', 'SciRagPaperQA2', 'REPO_DIR', 'SciRagDataSet', 'SciRag', 'TOP_K', 'DISTANCE_THRESHOLD', 'OAI_PRICE1K','SciRagGeminiAI','MistralOCRProcessor']
+__all__ = ['SciRagVertexAI', 'SciRagOpenAI', 'SciRagPaperQA2', 'REPO_DIR', 'SciRagDataSet', 'SciRag', 'TOP_K', 'DISTANCE_THRESHOLD', 'OAI_PRICE1K','SciRagHybrid','MistralOCRProcessor']
 
